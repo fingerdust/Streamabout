6ae954f07a86dfd2c4f1fd7262f11e621cb85212
Mark Tobin <mtspark@gmail.com>
2014-10-22 22:54:10 +0100

    Created header with animated logo. Responsive for medium and large but not mobile. Nav list in place.

--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,22 @@
 
 <div class="header">
-	<h1>HEADER TEST</h1>
-
-
-
+	  	<div class="twelve columns text-center logo">
+	  		<h1>
+			  	<a href="http://www.streamabout.com">
+				    <img class="logo-text" src="images/logo-text.png" alt="Streamabout" />
+				    <img class="logo-circles" src="images/logo-circles.png" alt="Streamabout" />
+				    <br />
+				    <img class="logo-subtext" src="images/logo-subtext.png" alt="Streamabout" />
+				</a>
+			</h1>
+		<ul class="nav">
+			  <li><a href="#">it's about you</a></li>
+			  <li><a href="#">what we do</a></li>
+			  <li><a href="#">our work</a></li>
+			  <li><a href="#">our clients</a></li>
+			  <li><a href="#">blog</a></li>
+			  <li><a href="#">contact</a></li>
+			</ul>
+		</div>
 </div>
+
