d6e7d391035e612b0878d3c691d01606e18f628a
Mark Tobin <mtspark@gmail.com>
2014-10-24 00:09:01 +0100

    Footer background and links

--- partials/footer.php
+++ partials/footer.php
@@ -1,6 +1,39 @@
 
 <div class="footer">
-	<h1>FOOTER</h1>
+	<div class="large-12 columns">
+		<div class="large-2 columns">
+			<a href="http://www.streamabout.com"><br /><br /><br /><br /><br /><br /><br />&copy; Streamabout Ltd 2014</a>
+		</div>		<div class="large-2 columns">
+			<ul>
+				<li><a href="#">Site Map</a></li>
+				<li><br /></li>
+				<li><a href="#">it's about you</a></li>
+				<li><a href="#">what we do</a></li>
+				<li><a href="#">our work</a></li>
+				<li><a href="#">our clients</a></li>
+				<li><a href="#">blog</a></li>
+				<li><a href="#">contact</a></li>
+			</ul>	
+		</div>		<div class="large-2 columns">
+			<ul>
+				<li><a href="#">Streamabout Limited</a></li>
+				<li><br /></li>
+				<li><a href="#">The Courtyard</a></li>
+				<li><a href="#">St Patrick's Road</a></li>
+				<li><a href="#">Dalkey</a></li>
+				<li><a href="#">County Dublin</a></li>
+				<li><a href="#">087123456</a></li>
+				<li><a href="#">hello@streamabout.ie</a></li>
+			</ul>
+		</div>
+		<div class="large-4 columns">
+			<a href="#"><br /><br /><br /><br /><br /><br /><br />Design: John Tobin / </a>
+			<a href="#">Development: Mark Tobin</a>
+		</div>
+		<div class="large-2 columns">
+
+		</div>
+	</div>
 
 
 
