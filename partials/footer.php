9ded9683bbf3da81241710a65d7478f5e6cc5d50
Mark Tobin <mtspark@gmail.com>
2014-10-25 23:11:41 +0100

    Added sprite to footer and made mobile versions of header and footer + fixes for medium footer.

--- partials/footer.php
+++ partials/footer.php
@@ -1,11 +1,33 @@
 
 <div class="footer">
-	<div class="large-12 columns">
-		<div class="large-2 columns">
-			<a href="http://www.streamabout.com"><br /><br /><br /><br /><br /><br /><br />&copy; Streamabout Ltd 2014</a>
-		</div>		<div class="large-2 columns">
+	<div class="twelve columns text-center mobile logo">
+  		<h1>
+		  	<a href="http://www.streamabout.com">
+			    <img class="logo-text" src="images/logo-text.png" alt="Streamabout" />
+			    <img class="logo-circles" src="images/logo-circles.png" alt="Streamabout" />
+			    <br />
+			    <img class="logo-subtext" src="images/logo-subtext.png" alt="Streamabout" />
+			</a>
+		</h1>
+		<div class="social-media">
+
+			<a href="#"><img id="facebook" src="images/sprites/trans.png"></a>
+			<a href="#"><img id="twitter" src="images/sprites/trans.png"></a>
+			<a href="#"><img id="linkedin" src="images/sprites/trans.png"></a>
+			<a href="#"><img id="youtube" src="images/sprites/trans.png"></a>
+			<a href="#"><img id="google" src="images/sprites/trans.png"></a>
+			<a href="#"><img id="pinterest" src="images/sprites/trans.png"></a>
+		</div>
+		<a href="#"><h3>Contact</h3></a>
+	</div>
+	<div class="large-12 medium-12 small-12 columns non-mobile">
+		<div class="large-2 columns non-medium">
+			</br></br></br></br></br></br></br>
+			<a href="http://www.streamabout.com">&copy; Streamabout 2014</a>
+		</div>		
+		<div class="large-2 medium-3 columns non-mobile">
 			<ul>
-				<li><a href="#">Site Map</a></li>
+				<li><span>Site Map</span></li>
 				<li><br /></li>
 				<li><a href="#">it's about you</a></li>
 				<li><a href="#">what we do</a></li>
@@ -14,24 +36,35 @@
 				<li><a href="#">blog</a></li>
 				<li><a href="#">contact</a></li>
 			</ul>	
-		</div>		<div class="large-2 columns">
+		</div>		
+		<div class="large-2 medium-4 columns non-mobile">
 			<ul>
-				<li><a href="#">Streamabout Limited</a></li>
+				<li><a href="http://www.streamabout.com">Streamabout Limited</a></li>
 				<li><br /></li>
-				<li><a href="#">The Courtyard</a></li>
-				<li><a href="#">St Patrick's Road</a></li>
-				<li><a href="#">Dalkey</a></li>
-				<li><a href="#">County Dublin</a></li>
-				<li><a href="#">087123456</a></li>
+				<li><span>The Courtyard</span></li>
+				<li><span>St Patrick's Road</span></li>
+				<li><span>Dalkey</span></li>
+				<li><span>County Dublin</span></li>
+				<li><span>087123456</span></li>
 				<li><a href="#">hello@streamabout.ie</a></li>
 			</ul>
 		</div>
-		<div class="large-4 columns">
-			<a href="#"><br /><br /><br /><br /><br /><br /><br />Design: John Tobin / </a>
-			<a href="#">Development: Mark Tobin</a>
+		<div class="large-3 columns non-mobile non-medium">
+			</br></br></br></br></br></br></br>
+			<span>Design: </span><a href="#">John Tobin</a><span> / </span>
+			<span>Dev: </span><a href="#">Mark Tobin</a>
 		</div>
-		<div class="large-2 columns">
+		<div class="large-3 medium-5 small-6 columns">
 
+			<div class="social-media">
+
+				<a href="#"><img id="facebook" src="images/sprites/trans.png"></a>
+				<a href="#"><img id="twitter" src="images/sprites/trans.png"></a>
+				<a href="#"><img id="linkedin" src="images/sprites/trans.png"></a>
+				<a href="#"><img id="youtube" src="images/sprites/trans.png"></a>
+				<a href="#"><img id="google" src="images/sprites/trans.png"></a>
+				<a href="#"><img id="pinterest" src="images/sprites/trans.png"></a>
+			</div>
 		</div>
 	</div>
 
